383363977712ad9bcbafb591d5d6e079fc421ae7
gyuu-neves <gyupro5@gmail.com>
2021-04-08 23:59:54 -0300

    added pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,69 +1,79 @@
-import { Flex, Text, Input, Icon, HStack } from '@chakra-ui/react'
-import { RiNotificationLine, RiSearchLine, RiUserAddFill } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddFill,
+} from 'react-icons/ri';
 
 export function Header() {
-    return (
-        <Flex 
-            as="header"    
-            w="100%"
-            maxWidth={1480}
-            h="20"
-            mx="auto"
-            mt="4"
-            px="6"
-            align="center"
-        >
-            <Text 
-                fontSize="3xl" 
-                fontWeight="bold" 
-                letterSpacing="tight" 
-                w="64"
-            >
-                dashgo
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center">
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        dashgo
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
 
-                <Text 
-                    as="span"
-                    ml="1"
-                    color="pink.500"
-                    >       
-                    .
-                </Text>
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full">
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          placeholder="Buscar na plataforma"
+          _placeholder={{ color: 'gray.400' }}
+        />
 
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700">
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddFill} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box>
+            <Text mr="4" textAlign="right">
+              Gyuber Neves
             </Text>
+            <Text color="gray.300" fontSize="small">
+              email@email.com
+            </Text>
+          </Box>
 
-            <Flex 
-                as="label"
-                flex="1"
-                py="4"
-                px="8" 
-                ml="6"           
-                maxWidth={400}
-                alignSelf="center"
-                color="gray.200"
-                position="relative"
-                bg="gray.800"
-                borderRadius="full"
-            >
-                <Input 
-                    color="gray.50"
-                    variant="unstyled"
-                    placeholder="Buscar na plataforma"
-                    _placeholder={{ color: 'gray.400' }}
-                />
-                
-                <Icon as={ RiSearchLine } fontSize="20"/>
-            </Flex>
-
-            <Flex 
-                align="center"
-                ml="auto"
-            >
-                <HStack spacing="4">
-                    <Icon as={RiNotificationLine} fontSize="20" />
-                    <Icon as={RiUserAddFill} fontSize="20" />
-                </HStack>
-            </Flex>
-
+          <Avatar
+            size="md"
+            nome="Gyuu Neves"
+            src="https://github.com/gyuu-neves.png"
+          />
         </Flex>
-    )
+      </Flex>
+    </Flex>
+  );
 }
